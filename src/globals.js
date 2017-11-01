4be41e55f0ed7b472982eb0663972bd7922dac7f
nextgensparx <siphomateke@gmail.com>
2017-11-01 20:58:35 +0200

    Move lists to Store and remove entities

--- src/globals.js
+++ src/globals.js
@@ -1,8 +1,7 @@
-export let creatures = [];
-export let foods = [];
-export let entities = [];
-
 export let Store = {
   maxEntityId: 0,
   maxFoodId: 0,
+  highestScore: 0,
+  creatures: [],
+  foods: [],
 };
